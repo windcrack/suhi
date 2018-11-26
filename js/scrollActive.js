f8ac3451e4795f02c6eaa0d55b4d8a72ae82ee4f
Alexander Rijkov <windcrack@mail.ru>
2018-11-26 19:23:30 +0300

    26-11 19:23

--- js/scrollActive.js
+++ js/scrollActive.js
@@ -44,32 +44,3 @@ $(window).scroll(function(){
    }                   
 });
 
-
-// jQuery(window).scroll(function(){
-//          var $sections = $('div');
-// 	$sections.each(function(i,el){
-//         var top  = $(el).offset().top-1;
-//         var bottom = top +$(el).height();
-//         var scroll = $(window).scrollTop();
-//         var id = $(el).attr('id');
-//     	if( scroll > top && scroll < bottom){
-//             $('a.active-menu').removeClass('active-menu');
-// 			$('a[href="#'+id+'"]').addClass('active-menu');
-
-//         }
-//     })
-//  });
-
-// $("nav").on("click","a", function (event) {
-//         // исключаем стандартную реакцию браузера
-//         event.preventDefault();
- 
-//         // получем идентификатор блока из атрибута href
-//         var id  = $(this).attr('href'),
- 
-//         // находим высоту, на которой расположен блок
-//             top = $(id).offset().top;
-         
-//         // анимируем переход к блоку, время: 800 мс
-//         $('body,html').animate({scrollTop: top}, 800);
-//     });
